a5e18475ce219910de48905bf7fe88a064efca6d
HorizonCode <horizoncode88@gmail.com>
2023-12-05 12:16:35 +0100

    add body parsing

--- routes/api/v1/posts/[postId].ts
+++ routes/api/v1/posts/[postId].ts
@@ -1,13 +1,29 @@
 import { H3Event } from "h3";
 import { addPost, getPosts } from "../../../../db/database";
+import assert from "assert";
 
 export default eventHandler(async (e: H3Event) => {
-  if (e.method === "PUT") {
-    const insert = addPost(e.context.params.postId as string, "test", "test");
-    return insert;
-  }
-  const postId = e.context.params.postId;
+  try {
+    if (e.method === "PUT") {
+      let body = await readBody(e);
 
-  const posts = getPosts(postId);
-  return posts;
+      assert((typeof body == "object"), "invalid body reply");
+
+      assert("post" in body, "key 'post' is missing");
+      assert("user" in body, "key 'user' is missing");
+      assert("comment" in body, "key 'comment' is missing");
+
+      const insert = addPost(e.context.params.postId as string, body.user, body.comment);
+      return insert;
+    }
+    const postId = e.context.params.postId;
+
+    const posts = getPosts(postId);
+    return posts;
+  } catch (err) {
+    return {
+      code: 500,
+      message: err.message
+    }
+  }
 });
