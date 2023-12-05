256cb57f161e5d517a876ba4acab61e243a20c8e
HorizonCode <horizoncode88@gmail.com>
2023-12-05 16:02:03 +0100

    remove unused config vars

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,15 +1,2 @@
 //https://nitro.unjs.io/config
-export default defineNitroConfig({
-  storage: {
-    cache: {
-      driver: "fs",
-      base: './data/cache',
-    }
-  },
-  devStorage: {
-    cache: {
-      driver: "fs",
-      base: "./data/cache"
-    }
-  }
-});
+export default defineNitroConfig({});
